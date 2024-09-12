df7c14d278b099a74098bbf4b31e0b44b065f2be
ViniciusSotana <vinysotana@gmail.com>
2024-09-12 17:02:23 -0300

    12-09-atualização

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ struct paises{
     string nome;
 };
 
-struct indicePaises{
+struct indices{
     int codigo;
     int ender;
 };
@@ -19,21 +19,110 @@ struct cidades{
     int codigoPais;
 };
 
-struct indiceCidades{
+struct guias{
     int codigo;
-    int ender;
+    string nome;
+    string endereco;
+    string telefone;
+    int codigoCidade;
 };
 
-struct Guias{
-    int codigo
-    string nome
-    endereco
-    telefone
-    codigoCidade;
+struct clientes{
+    int codigo;
+    string nome;
+    string endereco;
+    int codigoCidade;
 };
+
+
+struct pacotes{
+    int codigo;
+    string descricao;
+    int codigoGuia;
+    float valorPessoa;
+    int totalParticipantes;
+    int quantMaxPartipantes;
+};
+
+
+struct vendas{
+    int codigo;
+    int codigoCliente;
+    int codigoPacote;
+    int quantidadePessoas;
+    float valorTotal;
+};
+
+void organizaIndice(struct indices indice[], int tamanho){
+    for(i = 0; i < tamanho; i++) {
+        for(j = 0; j < tamanho-1; j++) {
+          if(indice[j] < indice[j + 1]) {
+            aux = indice[j];
+            indice[j] = indice[j + 1];
+            indice[j + 1] = aux;
+      }
+    }
+  }
+}
+
+void lerPaises(int tamanho, struct paises vetPaises[], struct indices indice[]){
+    cout << "\n\t\tFuncao para leitura de paises" << endl;
+    for(int i = 0, saida = 1; i < tamanho && saida != 0; i++){
+        cout << "\n\tPais " << i << endl;
+        cout << "Codigo: ";
+        cin >> vetPaises[i]codigo;
+        while(vetPaises[i].codigo <= 0){
+            cout << "\n\tDigite um codigo valido: ";
+            cin >> vetPaises[i]codigo;
+        }
+        indice[i].codigo = vetPaises[i].codigo;
+        indice[i].ender = i
+        cout << "\tNome: ";
+        cin >> vetPaises[i].nome;
+
+        cout << "\n\tDeseja continuar?(1/0): ";
+        cin >> saida;
+    }
+    organizaIndice(indicePais, tamanho);
+}
+
+void lerCidades(int tamanho, struct cidades vetCidade[], struct indices indice[]){
+    cout << "\n\t\tFuncao para leitura de Cidades" << endl;
+    for(int i = 0, saida = 1; i < tamanho && saida != 0; i++){
+        cout << "\n\tCidade " << i << endl;
+        cout << "Codigo: ";
+        cin >> vetCidade[i]codigo;
+        while(vetCidade[i].codigo <= 0){
+            cout << "\n\tDigite um codigo valido: ";
+            cin >> vetCidade[i]codigo;
+        }
+        indice[i].codigo = vetCidade[i].codigo;
+        indice[i].ender = i
+        cout << "\tNome: ";
+        cin >> vetCidade[i].nome;
+        cout << "\tEndereco: ";
+        cin >> vetCidade[i].endereco;
+        cout << "\tCodigo do pais: ";
+        cin >> vetCidade[i].codigoPais;
+
+        cout << "\n\tDeseja continuar?(1/0): ";
+        cin >> saida;
+    }
+    organizaIndice(indiceCidades, tamanho);
+}
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    bool menu = true;
+
+    while(menu==true){
+
+
+
+
+
+       cout << "\n\t\tDeseja continuar no programa?(1/0): ";
+       cin >> menu;
+    }
     return 0;
 }
